83ec8c012b50f0831862fca6c0482a0bc4e13881
Jaesu Han <84672164+Jaesu26@users.noreply.github.com>
2022-10-01 17:37:21 +0900

    Update functional.py

--- text_augmentation/functional.py
+++ text_augmentation/functional.py
@@ -17,16 +17,6 @@ Sentence = str
 Text = str
 
 
-def swap_sentences(sentences: List[Sentence]) -> List[Sentence]:
-    """swap two sentences"""
-    if len(sentences) < 2:
-        return sentences
-
-    idx1, idx2 = np.random.randint(len(sentences), size=2, replace=False)
-    sentences[idx1], sentences[idx2] = sentences[idx2], sentences[idx1]
-    return sentences
-
-
 def swap_words(words: List[Word]) -> List[Word]:
     """swap two words"""
     if len(words) < 2:
@@ -37,24 +27,14 @@ def swap_words(words: List[Word]) -> List[Word]:
     return words
 
 
-def delete_sentences(sentences: List[Sentence], min_sentences: int, deletion_prob: float) -> List[Sentence]:
-    """delete random sentences"""
-    num_sentences = len(sentences)
-
-    if num_sentences <= min_sentences:
+def swap_sentences(sentences: List[Sentence]) -> List[Sentence]:
+    """swap two sentences"""
+    if len(sentences) < 2:
         return sentences
 
-    new_sentences = []
-    deletion_counts = 0
-    deletion_max_counts = num_sentences - min_sentences
-    
-    for sentence in sentences:
-        if np.random.random() < deletion_prob and deletion_counts < deletion_max_counts:
-            deletion_counts += 1
-            continue
-
-        new_sentences.append(sentence)
-    return new_sentences
+    idx1, idx2 = np.random.randint(len(sentences), size=2, replace=False)
+    sentences[idx1], sentences[idx2] = sentences[idx2], sentences[idx1]
+    return sentences
 
 
 def delete_words(words: List[Word], min_words: int, deletion_prob: float) -> List[Word]:
@@ -75,3 +55,23 @@ def delete_words(words: List[Word], min_words: int, deletion_prob: float) -> Lis
             
         new_words.append(word)
     return new_words
+
+
+def delete_sentences(sentences: List[Sentence], min_sentences: int, deletion_prob: float) -> List[Sentence]:
+    """delete random sentences"""
+    num_sentences = len(sentences)
+
+    if num_sentences <= min_sentences:
+        return sentences
+
+    new_sentences = []
+    deletion_counts = 0
+    deletion_max_counts = num_sentences - min_sentences
+    
+    for sentence in sentences:
+        if np.random.random() < deletion_prob and deletion_counts < deletion_max_counts:
+            deletion_counts += 1
+            continue
+
+        new_sentences.append(sentence)
+    return new_sentences
