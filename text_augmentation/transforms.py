7c7647102102a059a8b79aaa57b22d9422580ac2
Jaesu Han <84672164+Jaesu26@users.noreply.github.com>
2022-10-02 02:22:24 +0900

    Update transforms.py

--- text_augmentation/transforms.py
+++ text_augmentation/transforms.py
@@ -16,7 +16,7 @@ __all__ = [
 
 
 class TextTransform(BasicTransform):
-    """Transform applied to text"""
+    """Transform applied to a text"""
 
     @property
     def targets(self) -> Dict[str, Callable[[Text], Text]]:
@@ -51,12 +51,18 @@ class TextTransform(BasicTransform):
 class RandomSwapWords(TextTransform):
     """Randomly swap two words in a random sentence"""
 
-    def __init__(self, always_apply: bool = False, p: float = 0.5) -> None:
+    def __init__(
+        self, 
+        ignore_first: bool = True,
+        always_apply: bool = False, 
+        p: float = 0.5
+    ) -> None:
         super(RandomSwapWords, self).__init__(always_apply, p)
+        self.ignore_first = ignore_first
 
     def apply(self, text: Text, **params: Any) -> Text:
         sentences = self.get_sentences_from_text(text)
-        idx = np.random.randint(len(sentences))
+        idx = np.random.randint(self.ignore_first, len(sentences))
         words = self.get_words_from_sentence(sentences[idx])
         words = F.swap_words(words)
         sentence = self.get_sentence_from_words(words)
@@ -64,24 +70,30 @@ class RandomSwapWords(TextTransform):
         text = self.get_text_from_sentences(sentences)
         return text
 
-    def get_transform_init_args_names(self) -> Tuple[()]:
-        return ()
+    def get_transform_init_args_names(self) -> Tuple[str]:
+        return ("ignore_first",)
 
     
 class RandomSwapSentences(TextTransform):
     """Randomly swap two sentences in the text"""
 
-    def __init__(self, always_apply: bool = False, p: float = 0.5) -> None:
+    def __init__(
+        self, 
+        ignore_first: bool = True,
+        always_apply: bool = False, 
+        p: float = 0.5
+    ) -> None:
         super(RandomSwapSentences, self).__init__(always_apply, p)
+        self.ignore_first = ignore_first
 
     def apply(self, text: Text, **params: Any) -> Text:
         sentences = self.get_senteces_from_text(text)
-        sentences = F.swap_sentences(sentences)
+        sentences = F.swap_sentences(sentences, self.ignore_sentence)
         text = self.get_text_from_sentences(sentences)
         return text
 
-    def get_transform_init_args_names(self) -> Tuple[()]:
-        return ()
+    def get_transform_init_args_names(self) -> Tuple[str]:
+        return ("ignore_first",)
 
 
 class RandomDeletionWords(TextTransform):
@@ -91,6 +103,7 @@ class RandomDeletionWords(TextTransform):
         self, 
         min_words_each_sentence: int = 5,
         deletion_prob: float = 0.1, 
+        ignore_first: bool = True,
         always_apply: bool = False, 
         p: float = 0.5
     ) -> None:
@@ -101,11 +114,13 @@ class RandomDeletionWords(TextTransform):
         
         self.min_words_each_sentence = min_words_each_sentence
         self.deletion_prob = deletion_prob
+        self.ignore_first = ignore_first
 
     def apply(self, text: Text, **params: Any) -> Text:
-        new_sentences = []
+        new_sentences = [sentences[0]] if self.ignore_first else [] 
         sentences = self.get_senteces_from_text(text)
-        for sentence in sentences:
+        
+        for sentence in sentences[self.ignore_first:]:
             words = self.get_words_from_sentence(sentence)
             words = F.delete_words(words, self.min_words_each_sentence, self.delete_prob)
             new_sentence = self.get_sentence_from_words(words)
@@ -115,8 +130,8 @@ class RandomDeletionWords(TextTransform):
         text = self.get_text_from_sentences(new_sentences)
         return text
 
-    def get_transform_init_args_names(self) -> Tuple[str, str]:
-        return ("min_words_each_sentence", "deletion_prob")
+    def get_transform_init_args_names(self) -> Tuple[str, str, str]:
+        return ("min_words_each_sentence", "deletion_prob", "ignore_first")
 
 
 class RandomDeletionSentences(TextTransform):
@@ -125,7 +140,8 @@ class RandomDeletionSentences(TextTransform):
     def __init__(
         self, 
         min_sentences: int = 3,
-        deletion_prob: float = 0.1, 
+        deletion_prob: float = 0.1,
+        ignore_first: bool = True,
         always_apply: bool = False, 
         p: float = 0.5
     ) -> None:
@@ -136,15 +152,16 @@ class RandomDeletionSentences(TextTransform):
     
         self.min_sentences = min_sentences
         self.deletion_prob = deletion_prob
+        self.ignore_first = ignore_first
 
     def apply(self, text: Text, **params: Any) -> Text:
         sentences = self.get_senteces_from_text(text)
-        sentences = F.delete_sentences(sentences, self.min_sentences, self.deletion_prob)
+        sentences = F.delete_sentences(sentences, self.min_sentences, self.deletion_prob, self.ignore_first)
         text = self.get_text_from_sentences(sentences)
         return text
 
-    def get_transform_init_args_names(self) -> Tuple[str, str]:
-        return ("min_sentences", "deletion_prob")
+    def get_transform_init_args_names(self) -> Tuple[str, str, str]:
+        return ("min_sentences", "deletion_prob", "ignore_first")
 
     
 class DeletionFullstops(TextTransform):
