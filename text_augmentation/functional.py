2dc132597640d361255226db84db059af009008b
Jaesu Han <84672164+Jaesu26@users.noreply.github.com>
2023-01-24 00:59:07 +0900

    [Rename] util functions

--- text_augmentation/functional.py
+++ text_augmentation/functional.py
@@ -5,10 +5,10 @@ import numpy as np
 
 from .utils import (
     strip,
-    get_words_from_sentence,
-    get_sentences_from_text,
-    get_sentence_from_words,
-    get_text_from_sentences,
+    split_sentence,
+    split_text,
+    combine_words,
+    combine_sentences,
     Word,
     Sentence,
     Text
@@ -26,31 +26,31 @@ __all__ = [
 
 def swap_words(text: Text, ignore_first: bool) -> Text:
     """Randomly swap two words in a random sentence in the text"""
-    sentences = get_sentences_from_text(text)
+    sentences = split_text(text)
     if len(sentences) <= ignore_first + 1:
         return text
 
     idx = np.random.randint(ignore_first, len(sentences))
-    words = get_words_from_sentence(sentences[idx])
+    words = split_sentence(sentences[idx])
     if len(words) >= 2:    
         idx1, idx2 = np.random.choice(len(words), size=2, replace=False)
         words[idx1], words[idx2] = words[idx2], words[idx1]
 
-    sentence = get_sentence_from_words(words)
+    sentence = combine_words(words)
     sentences[idx] = sentence
-    text = get_text_from_sentences(sentences)
+    text = combine_sentences(sentences)
     return text
 
 
 def swap_sentences(text: Text, ignore_first: bool) -> Text:
     """Randomly swap two sentences in the text"""
-    sentences = get_sentences_from_text(text)
+    sentences = split_text(text)
     if len(sentences) < ignore_first + 2:
         return text
         
     idx1, idx2 = np.random.choice(np.arange(ignore_first, len(sentences)), size=2, replace=False)
     sentences[idx1], sentences[idx2] = sentences[idx2], sentences[idx1]
-    text = get_text_from_sentences(sentences)
+    text = combine_sentences(sentences)
     return text
 
 
@@ -61,11 +61,11 @@ def delete_words(
     ignore_first: bool
 ) -> Text:
     """Randomly delete words in the text"""
-    sentences = get_sentences_from_text(text)
+    sentences = split_text(text)
     new_sentences = [sentences[0]] if ignore_first else [] 
     
     for sentence in sentences[ignore_first:]:
-        words = get_words_from_sentence(sentence)
+        words = split_sentence(sentence)
         new_words = words
 
         if len(words) > min_words:
@@ -78,11 +78,11 @@ def delete_words(
                     continue
                 new_words.append(word)
 
-        new_sentence = get_sentence_from_words(new_words)
+        new_sentence = combine_words(new_words)
         if new_sentence:
             new_sentences.append(new_sentence)
 
-    text = get_text_from_sentences(new_sentences)
+    text = combine_sentences(new_sentences)
     return text
 
     
@@ -93,7 +93,7 @@ def delete_sentences(
     ignore_first: bool
 ) -> Text:
     """Randomly delete sentences in the text"""
-    sentences = get_sentences_from_text(text)
+    sentences = split_text(text)
     if len(sentences) <= min_sentences:
         return text
 
@@ -107,7 +107,7 @@ def delete_sentences(
             continue
         new_sentences.append(sentence)
 
-    text = get_text_from_sentences(new_sentences)
+    text = combine_sentences(new_sentences)
     return text
 
 
