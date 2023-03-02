224baec0f36efb507e75bdc1190bad9f00293215
Jaesu Han <84672164+Jaesu26@users.noreply.github.com>
2023-03-03 00:58:04 +0900

    change kwargs order

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 from typing import List
 
 
@@ -10,15 +10,15 @@ def get_install_requires(file_path: str = "requirements.txt") -> List[str]:
 setup(
     name="text_boost",
     version="1.0.0",
-    url="https://github.com/Jaesu26/text-boost",
-    license="MIT",
+    description="Text augmentation using albumentations",
+    long_description=open("README.md", encoding="utf-8").read(),
     author="Jaesu Han",
     author_email="gkswotn9753@gmail.com",
-    description="Text augmentation using albumentations package",
+    url="https://github.com/Jaesu26/text-boost",
+    license="MIT", 
     packages=find_packages(),
-    long_description=open("README.md", encoding="utf-8").read(),
-    zip_safe=False,
     include_package_data=True,
+    zip_safe=False,
     python_requires=">=3.6",
     install_requires=get_install_requires(),
 )
