baefc716f08ca8de9c996b886dbe73b64d6a04bc
gkswotn12345 <84672164+Jaesu26@users.noreply.github.com>
2023-03-18 22:06:30 +0900

    Rename variable "param_names" -> "params_names"

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -13,8 +13,8 @@ def test_empty_input_text(augmentation):
 @pytest.mark.parametrize("incorrect_probability", [2j, "0.0", None])
 def test_incorrect_probability_type(augmentation_with_probability, incorrect_probability):
     augmentation = augmentation_with_probability
-    param_names = augmentation.__init__.__code__.co_varnames
-    probability_params = {name: incorrect_probability for name in param_names if 'prob' in name}
+    params_names = augmentation.__init__.__code__.co_varnames
+    probability_params = {name: incorrect_probability for name in params_names if 'prob' in name}
     with pytest.raises(TypeError) as error_info:
         augmentation(**probability_params)
     assert "must be a real number between 0 and 1." in str(error_info.value)
@@ -23,8 +23,8 @@ def test_incorrect_probability_type(augmentation_with_probability, incorrect_pro
 @pytest.mark.parametrize("incorrect_probability", [-1.0, 2])
 def test_incorrect_probability_value(augmentation_with_probability, incorrect_probability):
     augmentation = augmentation_with_probability
-    param_names = augmentation.__init__.__code__.co_varnames
-    probability_params = {name: incorrect_probability for name in param_names if 'prob' in name}
+    params_names = augmentation.__init__.__code__.co_varnames
+    probability_params = {name: incorrect_probability for name in params_names if 'prob' in name}
     with pytest.raises(ValueError) as error_info:
         augmentation(**probability_params)
     assert "must be between 0 and 1" in str(error_info.value)
