36016219cedaf3455a11a21593f4082042576e81
gkswotn12345 <84672164+Jaesu26@users.noreply.github.com>
2024-08-21 02:58:28 +0900

    Remove useless statement

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -16,12 +16,11 @@ def test_augmentations_serialization(
     always_apply,
     p,
 ):
-    text = long_text
     augment = augmentation(ignore_first=ignore_first, always_apply=always_apply, p=p)
     serialized_augment = A.to_dict(augment)
     deserialized_augment = A.from_dict(serialized_augment)
     set_seed(seed)
-    augmented_data = augment(text=text)
+    augmented_data = augment(text=long_text)
     set_seed(seed)
-    deserialized_augmented_data = deserialized_augment(text=text)
+    deserialized_augmented_data = deserialized_augment(text=long_text)
     assert augmented_data["text"] == deserialized_augmented_data["text"]
