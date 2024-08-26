0445a25f5df3491b5d3368282f43665b00d77c0b
gkswotn12345 <84672164+Jaesu26@users.noreply.github.com>
2024-08-27 04:39:31 +0900

    Add way to get stopwords and wordnet

--- textmentations/corpora/corpora.py
+++ textmentations/corpora/corpora.py
@@ -1,10 +1,20 @@
 import json
 import os
 
-STOPWORDS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "corpora/stopwords.txt")
-with open(STOPWORDS_PATH, encoding="utf-8") as f:
-    STOPWORDS = frozenset(f.read().splitlines())
 
-WORDNET_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "corpora/wordnet.json")
-with open(WORDNET_PATH, encoding="utf-8") as f:
-    WORDNET = json.load(f)
+def get_stopwords():
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "corpora/stopwords.txt")
+    with open(path, encoding="utf-8") as f:
+        stopwords = frozenset(f.read().splitlines())
+    return stopwords
+
+
+def get_wordnet():
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "corpora/wordnet.json")
+    with open(path, encoding="utf-8") as f:
+        wordnet = json.load(f)
+    return wordnet
+
+
+STOPWORDS = get_stopwords()
+WORDNET = get_wordnet()
