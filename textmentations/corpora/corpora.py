decc6eab25c78e3e3816e21d44725a448971be46
gkswotn12345 <84672164+Jaesu26@users.noreply.github.com>
2024-08-28 15:05:21 +0900

    Add type hints

--- textmentations/corpora/corpora.py
+++ textmentations/corpora/corpora.py
@@ -1,15 +1,18 @@
 import json
 import os
+from typing import Dict, FrozenSet, List
+
+from .types import Word
 
 
-def get_stopwords():
+def get_stopwords() -> FrozenSet[Word]:
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "corpora/stopwords.txt")
     with open(path, encoding="utf-8") as f:
         stopwords = frozenset(f.read().splitlines())
     return stopwords
 
 
-def get_wordnet():
+def get_wordnet() -> Dict[Word, List[Word]]:
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "corpora/wordnet.json")
     with open(path, encoding="utf-8") as f:
         wordnet = json.load(f)
